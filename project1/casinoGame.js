6f56a0d3c576a0af15cb14de469815a562043e7f
Joe Fryd <joescf@gmail.com>
2016-06-08 18:23:36 -0400

    working on project

---
+++ project1/casinoGame.js
@@ -0,0 +1,39 @@
+//locaton of images
+var barImg = 'http://icons.iconarchive.com/icons/designcontest/casino/96/Bar-icon.png';
+var treeImg = 'https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg';
+var princeImg = 'http://images1.citypages.com/imager/u/745xauto/8239109/whatprinceate.jpg';
+var codingImg = 'https://tctechcrunch2011.files.wordpress.com/2015/04/codecode.jpg';
+var nixonElvisImg = 'http://www.ew.com/sites/default/files/styles/tout_image_612x380/public/i/2016/04/23/nixon-elvis.jpg?itok=_M29sjo5';
+var bTTFImg = 'http://o.aolcdn.com/hss/storage/midas/f47e67f6910889f436f2a0a2242e1134/202841652/nikemags2015.png';
+var manhattanImg = 'http://www.nyc-architecture.com/LM/aerial1.jpg';
+var garfieldImg = 'https://pbs.twimg.com/profile_images/451027744361422849/f5KCfGHa_400x400.jpeg';
+var goldImg = 'http://blog.gainesvillecoins.com/wp-content/uploads/2010/11/generic-gold-bars.jpg';
+var stressedImg = 'http://www.sciencedump.com/sites/default/files/styles/youtube_dimension/public/field/teaserimage/stress_0.jpg?itok=5S0ZC0FI';
+var jqueryImg = 'https://raw.githubusercontent.com/peterkokot/awesome-jquery/master/awesome-jquery.png';
+
+//array of images
+var images = [
+barImg,
+treeImg,
+princeImg,
+codingImg,
+nixonElvisImg,
+bTTFImg,
+manhattanImg,
+garfieldImg,
+goldImg,
+stressedImg,
+jqueryImg
+]
+
+$(document).ready(function() {
+})
+
+
+
+
+
+
+
+
+
